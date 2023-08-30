9e57a861a901d41b1033b639e61d67f55aa147b0
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2023-08-30 15:51:57 -0300

    starting

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,11 @@
+interface TweetProps {
+    user: string
+    children: string
+    likes?: number
+}
+
+export function Tweet(props: TweetProps) {
+    return (
+        <div></div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './global.css'
+
+import twitterLogo from './assets/logo-twitter.svg'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div className='layout'>
+      <aside className='sidebar'>
+        <img src={twitterLogo} alt="logo" />
+
+        <nav></nav>
+
+        <button>Tweet</button>
+      </aside>
+      <div className="content">
+        content
+      </div>
+    </div>
   </React.StrictMode>,
 )
