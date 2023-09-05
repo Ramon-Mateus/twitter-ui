80a1eab5c007a825a24b424317dcb5c5c8e56e0c
Ramon de Oliveira Araújo Mateus <60753390+Ramon-Mateus@users.noreply.github.com>
2023-09-05 18:38:38 -0300

    Feat: visual structure finish

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,11 +1,41 @@
-interface TweetProps {
-    user: string
-    children: string
-    likes?: number
-}
+import { ArrowsClockwise, ChatCircle, Heart } from 'phosphor-react'
+import './Tweet.css'
 
-export function Tweet(props: TweetProps) {
+export function Tweet() {
     return (
-        <div></div>
+        <a href="#" className="tweet">
+            <img src="https://github.com/Ramon-Mateus.png" alt="Ramon Mateus" />
+
+            <div className="tweet-content">
+                <div className="tweet-content-header">
+                    <strong>Ramon Mateus</strong>
+                    <span>@ramonmateus</span>
+                </div>
+
+                <p>
+                    Acabei de migrar um projeto React GIGANTE de create-react-app para Vite e os resultados foram: <br />
+                    <br />
+                    ✅ npm start: De 32s para 400ms (sim, demorava 30s) <br />
+                    ✅ npm build: De 120s para 22s<br />
+                    <br />
+                    Além disso, troquei do Yarn para o PNPM e o install das deps mudou de 24s para 8s 🔥
+                </p>
+
+                <div className="tweet-content-footer">
+                    <button type="submit">
+                        <ChatCircle />
+                        20
+                    </button>
+                    <button type="submit">
+                        <ArrowsClockwise />
+                        15
+                    </button>
+                    <button type="submit">
+                        <Heart />
+                        50
+                    </button>
+                </div>
+            </div>
+        </a>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, User } from 'phosphor-react'
+import { Bell, BookmarkSimple, DotsThreeCircle, Envelope, FileText, Hash, House, Sparkle, User } from 'phosphor-react'
 import './global.css'
 
 import twitterLogo from './assets/logo-twitter.svg'
+import { Tweet } from './components/Tweet'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -53,7 +54,28 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       </aside>
 
       <div className="content">
-        content
+        <main className="timeline">
+          <div className="timeline-header">
+            Home
+            <Sparkle />
+          </div>
+
+          <form className="new-tweet-form">
+            <label htmlFor="tweet">
+              <img src="https://github.com/Ramon-Mateus.png" alt="Ramon Mateus" />
+              <textarea id="tweet" placeholder="What's happening?" />
+            </label>
+
+            <button type="submit">Tweet</button>
+          </form>
+
+          <div className="separator" />
+
+          <Tweet />
+          <Tweet />
+          <Tweet />
+          <Tweet />
+        </main>
       </div>
 
     </div>
